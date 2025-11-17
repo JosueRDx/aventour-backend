3146b5a7c14f7c47340d7aba7361094291e6aa91
reluxe3 <yoelfabricio123@gmail>
2025-11-16 21:11:33 -0500

    Scafold p1

---
+++ Aventour.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aventour.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        
+        public DateTime? LastModifiedDate { get; set; }
+        
+        public bool IsActive { get; set; } = true; 
+    }
+}
